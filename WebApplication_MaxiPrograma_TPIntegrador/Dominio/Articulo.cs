b7d6d4a87db1ec02655b0e927983d4ed803f41b0
Maxi <vcmaxi@gmail.com>
2024-06-23 11:42:03 -0300

    Preparación de archivos para Elaboracion de TP

--- WebApplication_MaxiPrograma_TPIntegrador/Dominio/Articulo.cs
+++ WebApplication_MaxiPrograma_TPIntegrador/Dominio/Articulo.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
 
 namespace Dominio {
     public class Articulo {
@@ -20,7 +15,6 @@ namespace Dominio {
             imagenUrl=null;
             precio=0;
         }
-
         public int Id { get; set; }
         [DisplayName("Código")]
         public string Codigo { get; set; }
